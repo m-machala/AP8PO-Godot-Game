8ec900e77feba8a00f08cdd1d7f33a6894a06491
m-machala <114995208+m-machala@users.noreply.github.com>
2024-06-09 19:56:20 +0200

    add periodic food spawning

--- Levels/Level1/Level1.cs
+++ Levels/Level1/Level1.cs
@@ -4,9 +4,10 @@ using System.Collections.Generic;
 
 public partial class Level1 : Level
 {
-	public List<PackedScene> stationaryFoodScenes = new List<PackedScene>();
+	public List<(PackedScene, double)> stationaryFoodScenes = new List<(PackedScene, double)>();
 	public RandomNumberGenerator rng = new RandomNumberGenerator();
-	public static int maxFoodCount = 10;
+	public static int startingFoodCount = 10;
+	public double foodTimer = 0;
 	public override void _Ready()
 	{
 		playerInstance = (Player)GD.Load<PackedScene>("res://Player.tscn").Instantiate();
@@ -20,31 +21,41 @@ public partial class Level1 : Level
 
 		LoadFoodScenes();
 		
-		for(int i = 0; i < maxFoodCount; i++) {
+		for(int i = 0; i < startingFoodCount; i++) {
 			SpawnFood();
 		}
 	}
 
 	public override void _Process(double delta)
 	{
+		foodTimer += delta;
+		if((int)foodTimer >= 4) {
+			foodTimer -= 4;
+			SpawnFood();
+		}
+
 	}
 
 	public void LoadFoodScenes() {
-		stationaryFoodScenes.Add(GD.Load<PackedScene>("res://Levels/Level1/Food1.tscn"));
-		stationaryFoodScenes.Add(GD.Load<PackedScene>("res://Levels/Level1/Food2.tscn"));
-		stationaryFoodScenes.Add(GD.Load<PackedScene>("res://Levels/Level1/Food3.tscn"));
-		stationaryFoodScenes.Add(GD.Load<PackedScene>("res://Levels/Level1/Food4.tscn"));
-		stationaryFoodScenes.Add(GD.Load<PackedScene>("res://Levels/Level1/Food5.tscn"));
-		stationaryFoodScenes.Add(GD.Load<PackedScene>("res://Levels/Level1/Food6.tscn"));
+		stationaryFoodScenes.Add((GD.Load<PackedScene>("res://Levels/Level1/Food1.tscn"), 0.5));
+		stationaryFoodScenes.Add((GD.Load<PackedScene>("res://Levels/Level1/Food2.tscn"), 0.9));
+		stationaryFoodScenes.Add((GD.Load<PackedScene>("res://Levels/Level1/Food3.tscn"), 1.1));
+		stationaryFoodScenes.Add((GD.Load<PackedScene>("res://Levels/Level1/Food4.tscn"), 1.3));
+		stationaryFoodScenes.Add((GD.Load<PackedScene>("res://Levels/Level1/Food5.tscn"), 1.5));
+		stationaryFoodScenes.Add((GD.Load<PackedScene>("res://Levels/Level1/Food6.tscn"), 1.7));
 	}
 
 	public void SpawnFood() {
-		var newFoodIndex = rng.RandiRange(0, stationaryFoodScenes.Count - 1);
-		var newFood = (Edible)stationaryFoodScenes[newFoodIndex].Instantiate();
-		newFood.Connect("Eaten", new Callable(this, nameof(SpawnFood)));
+		var newFoodIndex = -1;
+		for(int i = 0; i < 3; i++) {
+			if(newFoodIndex < 0 || stationaryFoodScenes[newFoodIndex].Item2 > playerInstance.size - 0.3) {
+				newFoodIndex = rng.RandiRange(0, stationaryFoodScenes.Count - 1);
+			}
+		}
+		var newFood = (Edible)stationaryFoodScenes[newFoodIndex].Item1.Instantiate();
 		var viewportSize = GetViewport().GetVisibleRect().Size;
-		newFood.Position = new Vector2(rng.RandiRange(0, (int)viewportSize.X), rng.RandiRange(0, (int)viewportSize.Y));
-		newFood.size = 0.5f;
+		newFood.Position = new Vector2(rng.RandiRange(0, (int)viewportSize.X - 1), rng.RandiRange(0, (int)viewportSize.Y - 1));
+		newFood.size = (float)stationaryFoodScenes[newFoodIndex].Item2;
 		AddChild(newFood);
 	}
 
