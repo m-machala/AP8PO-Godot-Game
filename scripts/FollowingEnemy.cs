a8ff5f9302e3281400109c7bd771bae3a104c9ea
m-machala <114995208+m-machala@users.noreply.github.com>
2024-06-09 16:42:14 +0200

    change FollowingEnemy scared logic

--- scripts/FollowingEnemy.cs
+++ scripts/FollowingEnemy.cs
@@ -6,11 +6,21 @@ public partial class FollowingEnemy : Edible
 	public float speed = 50f;
 	public override void _PhysicsProcess(double delta)
 	{
-		var movementVector = getPlayerPosition() - Position;
+		var playerPosition = getPlayerPosition();
+		var movementVector = playerPosition - Position;
 
 		if(getPlayerSize() > size + 0.2) {
 			movementVector *= -1;
+
+			var distanceFromPlayer = Math.Sqrt(Math.Pow(movementVector.X, 2) + Math.Pow(movementVector.Y, 2));
+			if(distanceFromPlayer > 250) {
+				movementVector = GetViewport().GetVisibleRect().Size / 2 - Position;
+			}
+			else if(distanceFromPlayer > 200) {
+				movementVector = Vector2.Zero;
+			}
 		}
+
 
 		var collideResult = MoveAndCollide(movementVector.Normalized() * speed * (float)delta);
 
