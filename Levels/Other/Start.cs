391906e7c4a418d710a67d2ccf91eb7abe91d485
Martin Masar <m_masar@utb.cz>
2024-06-13 10:07:00 +0200

    BG+BGM+scaled

--- Levels/Other/Start.cs
+++ Levels/Other/Start.cs
@@ -6,14 +6,14 @@ public partial class Start : Control
 	private Button buttonStart;
 	private Button buttonQuit;
 
-    public override void _Ready()
-    {
-        buttonStart = GetNode<Button>("MarginContainer/VBoxContainer/Start");
+	public override void _Ready()
+	{
+		buttonStart = GetNode<Button>("MarginContainer/VBoxContainer/Start");
 		buttonQuit = GetNode<Button>("MarginContainer/VBoxContainer/Quit");
 
 		buttonStart.Connect("pressed", new Callable(this, nameof(StartPressed)));
 		buttonQuit.Connect("pressed", new Callable(this, nameof(QuitPressed)));
-    }
+	}
 
 	public void StartPressed() {
 		((game)GetParent()).StartGame();
